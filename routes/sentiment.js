7fd4faf07650fe0888989e08f6e54fbb33cce845
Quan Pham <quanmp@bu.edu>
2022-09-25 10:42:15 -0400

    Added Google NLP

--- routes/sentiment.js
+++ routes/sentiment.js
@@ -5,20 +5,85 @@ const crypto = require('crypto');
 const qs = require('querystring')
 
 var router = express.Router();
+const bearerToken = process.env.TWITTER_BEARER_TOKEN;
+const getIdURL = "https://api.twitter.com/2/users/by"
+
+async function getUserID(username) {
+    let params = {
+        "usernames" : username
+    }
+
+    const options = {
+        headers: {
+            "User-Agent": "v2QuoteTweetsJS",
+            "authorization": `Bearer ${bearerToken}`
+        }
+    }
+
+    const resp = await needle('get', getIdURL, params, options);
+    return resp.body["data"][0]["id"].toString();
+
+};
+
+async function getUserComment(uid) {
+    // Given user's id, get the user's 100 most recent tweets
+
+    let userComments = Array();
+    const getTweetsURL = `https://api.twitter.com/2/users/${uid}/tweets`
+
+    let params = {
+        "tweet.fields" : "conversation_id,created_at",
+        "max_results" : 100
+    }
+
+    const options = {
+        headers: {
+            "User-Agent": "v2QuoteTweetsJS",
+            "authorization": `Bearer ${bearerToken}`
+        }
+    }
+
+    let resp = await needle('get', getTweetsURL, params, options);
+    
+    for (const comment of resp.body["data"]) {
+        // Push in comment object, id and all
+        userComments.push(comment["text"]);
+    }
+
+    console.log(userComments[0])
+    return userComments;
+
+}
 
 async function getSentiment(tweets) {
+    // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
+
+    // Instantiates a client
+    const client = new language.LanguageServiceClient();
+
     // tweets is an Array() of strings, each string is a user's tweet
     // For now, use the following as a test array:
-    let tweetsArray = Array();
-    let sentiment = 0;
-    tweetsArray.push(["Hello", "I studied NZ fur seals for years and the babies will lie on the rocks with just their face in the water blowing bubbles all day <3", "Good Lord, that is wonderful."])
+    const text = tweets.join("\n\n");
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeSentiment({document: document});
+    let sentiment = result.documentSentiment;
+    console.log(sentiment)
 
     return sentiment
 }
 
-router.get('/', (req, res, next) => {
-
-    res.send("Bot detecting!");
+router.get('/', async (req, res, next) => {
+    let username = req.query.follower;
+    let uid = await getUserID(username);
+    let tweets = await getUserComment(uid);
+    let sentiment = await getSentiment(tweets)
+    res.send("Bot detecting! Sentiment score is " + sentiment.score);
 })
 
 module.exports = router;
