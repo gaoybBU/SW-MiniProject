beb8d5a9badf685bb7c64ace889a663e629b9807
gaoybBU <gaoyb@bu.edu>
2022-09-24 21:04:31 -0400

    Google NLP update

--- routes/test.js
+++ routes/test.js
@@ -8,21 +8,22 @@ const language = require('@google-cloud/language');
 var router = express.Router();
 let totalScore = 0;
 
-async function getSentiment(tweets) {
+async function getSentiment() {
     // tweets is an Array() of strings, each string is a user's tweet
     // For now, use the following as a test array:
     let tweetsArray = Array();
     let sentiment = 0;
-    tweetsArray.push(["Hello", "I studied NZ fur seals for years and the babies will lie on the rocks with just their face in the water blowing bubbles all day <3", "Good Lord, that is wonderful."])
+    tweetsArray.push(["Fuck you", "I studied NZ fur seals for years and the babies will lie on the rocks with just their face in the water blowing bubbles all day <3", "Good Lord, that is wonderful."])
 
     tweetsArray.forEach(analyizer);
-    
-    return totalScore/tweetsArray.length();
+    output = totalScore/tweetsArray.length; // average score
+    console.log(output);
+    return output;   
 }
 
 async function analyizer(text) {
     // analyises the sentiment for a string of text
-    const client = new language.languageServiceClient();
+    const client = new language.LanguageServiceClient();
     const document = {
         content: text,
         type: 'PLAIN_TEXT',
@@ -32,7 +33,11 @@ async function analyizer(text) {
     const [result] = await client.analyzeSentiment({document: document});
     const sentiment = result.documentSentiment;
     totalScore += sentiment.score;
+    console.log(totalScore);
 }
+
+
+console.log(getSentiment().then((res)=>{console.log(res)}));
 
 router.get('/', (req, res, next) => {
 
@@ -40,4 +45,3 @@ router.get('/', (req, res, next) => {
 })
 
 module.exports = router;
-
